a0a031364872b147736533b71022afea179a5ac6
Max Rydahl Andersen <max.andersen@gmail.com>
2022-05-05 17:22:58 +0200

    add java 18 specific

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -11,7 +11,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello from RESTEasy Reactive " + formatterPatternSwitch(34);
     }
 
     static String formatterPatternSwitch(Object o) {
